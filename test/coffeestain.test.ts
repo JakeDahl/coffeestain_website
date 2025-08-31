1ccb15d0c38dfc5d1544b23da0240eb050d32e83
jakedahl <jakejdahl@gmail.com>
2025-08-31 06:13:26 -0500

    Initial commit

---
+++ test/coffeestain.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Coffeestain from '../lib/coffeestain-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/coffeestain-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Coffeestain.CoffeestainStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
